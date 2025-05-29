a83208b4d71a82622f4aef7a94e21087ec2b099f
DaggerInput <115953983+DaggerInput@users.noreply.github.com>
2025-05-29 16:54:11 -0700

    Update predict_stock.py

--- predict_stock.py
+++ predict_stock.py
@@ -2,6 +2,14 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import sys
+import os
+if len(sys.argv) > 1:
+    output_folder = sys.argv[1]
+else:
+    output_folder = os.getcwd()  
+
+output_path = os.path.join(output_folder, "stock_prediction.png")
 
 stock = yf.download('AAPL', period='60d', interval='1d')
 stock = stock.reset_index()
@@ -26,4 +34,5 @@ plt.legend()
 plt.grid(True)
 
 plt.tight_layout()
-plt.savefig('stock_prediction.png')
+plt.savefig(output_path)
+print(f"Image saved to: {output_path}")
